6c49f2574bcbcd77a5a7782783f1a322ebd50a48
maxcoh3n <maxcoh3n@gmail.com>
2021-02-09 14:25:49 -0800

    test change

--- source/test.js
+++ source/test.js
@@ -3,12 +3,12 @@ console.log("test")
 
 /**
  *
- * @param {number} floors
+ * @param {number} floors1
  * @param {number} bathrooms
  * @param {number} rooms
  * @param {string} street
  */
-function house( floors, bathrooms, rooms, street ) {
-    let ret = `This house on ${street} street has ${floors} floors, ${bathrooms} bathrooms, and ${rooms} rooms!`;
+function house( floors1, bathrooms, rooms, street ) {
+    let ret = `This house on ${street} street has ${floors1} floors, ${bathrooms} bathrooms, and ${rooms} rooms!`;
     return ret;
 }
