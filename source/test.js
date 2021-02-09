251e0369f1f4bb395e23a10899d0dc7cd6d9fb59
maxcoh3n <maxcoh3n@gmail.com>
2021-02-09 14:21:06 -0800

    add sample jsDocs test

--- source/test.js
+++ source/test.js
@@ -1,2 +1,14 @@
 console.log("test")
 console.log("test")
+
+/**
+ *
+ * @param {number} floors
+ * @param {number} bathrooms
+ * @param {number} rooms
+ * @param {string} street
+ */
+function house( floors, bathrooms, rooms, street ) {
+    let ret = `This house on ${street} street has ${floors} floors, ${bathrooms} bathrooms, and ${rooms} rooms!`;
+    return ret;
+}
