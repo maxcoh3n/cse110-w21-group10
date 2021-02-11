bab89f51e462efcc2b5ed98f21e3fc51ec8daf21
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-11 11:55:51 -0800

    Update timer.js

--- source/timer.js
+++ source/timer.js
@@ -1,13 +1,17 @@
+const startMins = 25;
+let time = startMins*60;
 
-var seconds = 0;
-var interval ;
-function pomodoro(mins) {
-   seconds = mins*60 || 0;     
-   interval = setInterval(function() {
-        seconds--;
-        if(!seconds){
-             clearInterval(interval); 
-             alert("🚨 It is Cool 😎. I wish you could share ");
-        }
-   },1000)
+const countdown = document.getElementByID('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+   const mins = Math.floor(time/60);
+   let sec = time%60;
+   
+   //sec = sec < 10
+   
+   countdown.innerHTML = `${mins}: ${sec}`;
+   time--;
+
 }
