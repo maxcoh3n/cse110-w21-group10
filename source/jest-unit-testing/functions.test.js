4933613e4f81fa47c624662a109c564fe353cee4
maxcoh3n <maxcoh3n@gmail.com>
2021-02-13 15:49:48 -0800

    test

--- source/jest-unit-testing/functions.test.js
+++ source/jest-unit-testing/functions.test.js
@@ -10,7 +10,7 @@ const functions = {
     expect(functions.add(1, 2)).toBe(3);
   });
 
-
+//hello
   test('subtracts 3 - 2 to equal 1', () => {
     expect(functions.subtract(3, 2)).toBe(1);
   });
