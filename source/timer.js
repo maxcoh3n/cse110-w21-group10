d1e8bc45b48a311c83cbc4f2f414cb8ac0f37d4e
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-14 17:46:18 -0800

    Update timer.js

--- source/timer.js
+++ source/timer.js
@@ -1,18 +1,23 @@
-let startingMins = 25;
-let time = startingMins * 60;
+function timer(startTime){
+  
+  let startingMins = startTime;
+  let time = startingMins * 60;
 
-const countdown = document.getElementById('countdown');
+  const countdown = document.getElementById('countdown');
 
-setInterval(updateCountdown, 1000);
+  setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
-  time = time < 0 ? 0 : time;
+  function updateCountdown() {
+    time = time < 0 ? 0 : time;
 
-  let mins = Math.floor(time/60);
-  let sec = time % 60;
+    let mins = Math.floor(time/60);
+    let sec = time % 60;
 
-  sec = sec < 10 ? '0' + sec : sec;
+    sec = sec < 10 ? '0' + sec : sec;
 
-  countdown.innerHTML = `${mins}:${sec}`;
-  time--;
+    countdown.innerHTML = `${mins}:${sec}`;
+    time--;
+  }
 }
+
+timer(25);
