7f2a1bd36a0a7e05d508496820e8b89863d0cae2
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-16 02:46:04 -0800

    Update timer.js

--- source/timer.js
+++ source/timer.js
@@ -1,7 +1,6 @@
 /**
 timer
 Uses the countdown h1 to set and run a timer of length designated by the startTime parameter.
-
 @param    startTime the length of the timer in minutes.
 */
 
@@ -26,4 +25,66 @@ function timer(startTime){
   }
 }
 
-timer(25);
+
+//Settings
+
+var modal = document.getElementById("myModal");
+var settingsBtn = document.getElementById("settingsBtn");
+var span = document.getElementsByClassName("close")[0];
+
+settingsBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const worktimeSlider = document.getElementById("worktime-slider");
+worktimeSlider.addEventListener('input', updateWorktimeValue);
+
+const worktimeNumber = document.getElementById('worktime-number');
+worktimeNumber.addEventListener('input', updateWorktimeValue);
+
+const breaktimeSlider = document.getElementById('breaktime-slider');
+breaktimeSlider.addEventListener('input', updateBreaktimeValue);
+
+const breaktimeNumber = document.getElementById('breaktime-number');
+breaktimeNumber.addEventListener('input', updateBreaktimeValue);
+
+const numbreaksSlider = document.getElementById('numbreaks-slider');
+numbreaksSlider.addEventListener('input', updateNumbreaksValue);
+
+const numbreaksNumber = document.getElementById('numbreaks-number');
+numbreaksNumber.addEventListener('input', updateNumbreaksValue);
+
+function updateWorktimeValue(e){
+  let num = e.target.value;
+  worktimeSlider.value = num;
+  worktimeNumber.value = num;
+}
+
+function updateBreaktimeValue(e){
+  let num = e.target.value;
+  breaktimeSlider.value = num;
+  breaktimeNumber.value = num;
+}
+
+function updateNumbreaksValue(e){
+  let num = e.target.value;
+  numbreaksSlider.value = num;
+  numbreaksNumber.value = num;
+}
+
+
+const startBtn = document.getElementById("startBtn");
+startBtn.onclick = function() {
+  timer(worktimeNumber.value);
+  startBtn.disabled = true;
+}
