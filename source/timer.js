48345c481bc1e40ce437e49b2c6347505cc0c829
Jerry Waldorf <jwaldorf@ucsd.edu>
2021-02-18 02:58:03 -0800

    Implemented cancel button

--- source/timer.js
+++ source/timer.js
@@ -1,5 +1,9 @@
 //Timer
 
+var count;
+const countdown = document.getElementById('countdown');
+const title = document.getElementById('title-countdown');
+
 /**
 timer
 Uses the countdown h1 to set and run a timer of length designated by the startTime parameter.
@@ -10,10 +14,7 @@ function timer(startTime){
 
   let time = startTime * 60;
 
-  const countdown = document.getElementById('countdown');
-  const title = document.getElementById('title-countdown');
-
-  setInterval(updateCountdown, 1000);
+  count = setInterval(updateCountdown, 1000);
 
   function updateCountdown() {
     time = time < 0 ? 0 : time;
@@ -34,4 +35,20 @@ const startBtn = document.getElementById("start-btn");
 startBtn.onclick = function() {
   timer(worktimeNumber.value);
   startBtn.disabled = true;
+  cancelBtn.disabled = false;
+}
+
+/**
+cancelBtn
+When cancel button is clicked while timer is in progress, countdown is set to 0:00
+*/
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.onclick = function() {
+  if( worktimeNumber.value > 0 && startBtn.disabled ) {
+    clearInterval(count);
+    title.innerHTML = "0:00";
+    countdown.innerHTML = "0:00";
+    startBtn.disabled = false;
+    cancelBtn.disabled = true;
+  }
 }
