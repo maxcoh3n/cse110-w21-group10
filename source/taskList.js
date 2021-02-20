217c72fd666f60e11de7694e7e239ca230da76df
maxcoh3n <maxcoh3n@gmail.com>
2021-02-20 10:54:58 -0800

    move functions from constant to taskList

--- source/taskList.js
+++ source/taskList.js
@@ -1,3 +1,36 @@
-/*
-Functions to render tasks dynamically, complete tasks, and add tasks should be here
-*/
+addTask.onclick = function() {                          // Don't know the button name on html yet
+    var tasks = [];
+    //tasks[numTasks] = prompt("Enter new task:");
+    if (tasks != null)
+    {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        tasks.push(JSON.parse(localStorage.getItem('tasks')));
+        //numtasks++;
+    }
+}
+completed.onclick = function() {
+    var completedTasks = [];
+    for (var i = 0; i < numtasks; i++)
+    {
+        completedTasks[i] = false;
+        if (document.getElementById(tasks[i]).checked == true)      //.condition for if tasks in checked
+            completedTasks[i] = true;                               // mark task as complete if condition is true. 
+    }
+}
+
+function logs()
+{
+    var today = new Date();
+    var day = String(today.getDate()).padStart(2, '0');
+    var month = String(today.getMonth() + 1).padStart(2, '0'); 
+    today = month + '/' + day + ': ';
+    for (var i = 0; i < numtasks; i++)
+    {
+        if (completedTasks[i])
+        {
+            console.log(today + tasks[i]);
+        }
+    }
+}
+//task list to do, and completed tasks along with date, rating, associated task
+//default empty array for completedTasks and upcomingTasks
