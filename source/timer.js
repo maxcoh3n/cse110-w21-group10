677469d9c0114b7050cfaaa180bb34073822caa3
Navid kazimi <nkaz5621@gmail.com>
2021-02-21 17:15:34 -0800

    new update

--- source/timer.js
+++ source/timer.js
@@ -4,6 +4,14 @@ const countdown = document.getElementById('countdown');
 const title = document.getElementById('title-countdown');
 const startBtn = document.getElementById("start-btn");
 
+/*sound*/
+const sound = document.getElementById("alarm-sound");
+function startMusic() {
+  sound.play();
+}
+
+
+
 /**
 timer
 Uses the countdown h1 to set and run a timer of length designated by the startTime parameter.
@@ -14,10 +22,14 @@ function timer(){
   if( startBtn.innerHTML == "Start" ) {
     const devMode = document.getElementById("dev-mode");
 
-    time = localStorage.getItem('workMins') * 60;
+    time = 5;
+    //time = localStorage.getItem('workMins') * 60;
     count = setInterval(updateCountdown, devMode.checked? 10: 1000);
     startBtn.innerHTML = "Cancel";
   } else {
+    sound.pause();
+    sound.currentTime = 0;
+    
     time = 0;
     clearInterval(count);
     title.innerHTML = "0:00";
@@ -26,6 +38,7 @@ function timer(){
   }
 
   function updateCountdown() {
+    
     time = time < 0 ? 0 : time;
 
     let mins = Math.floor(time/60);
@@ -36,6 +49,9 @@ function timer(){
     title.innerHTML = `${mins}:${sec}`
     countdown.innerHTML = `${mins}:${sec}`;
     time--;
+    if (time === 0) {
+      startMusic();
+    }
   }
 }
 
