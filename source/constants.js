9e318519be4838da3be650be30778c1fca9b3d07
Nir <nirbek@gmail.com>
2021-02-22 18:46:17 -0800

    changed to camelCase and double quotes

--- source/constants.js
+++ source/constants.js
@@ -1,24 +1,24 @@
-let workMins = localStorage.getItem('workMins');
-if (!workMins) localStorage.setItem('workMins', '25');
+let workMins = localStorage.getItem("workMins");
+if (!workMins) localStorage.setItem("workMins", "25");
 
-let shortBreakMins = localStorage.getItem('shortBreakMins');
-if (!shortBreakMins) localStorage.setItem('shortBreakMins', '5');
+let shortBreakMins = localStorage.getItem("shortBreakMins");
+if (!shortBreakMins) localStorage.setItem("shortBreakMins", "5");
 
-let longBreakMins = localStorage.getItem('longBreakMins');
-if (!longBreakMins) localStorage.setItem('longBreakMins', '15');
+let longBreakMins = localStorage.getItem("longBreakMins");
+if (!longBreakMins) localStorage.setItem("longBreakMins", "15");
 
-let numSessions = localStorage.getItem('numSessions');
-if (!numSessions) localStorage.setItem('numSessions', '4');
+let numSessions = localStorage.getItem("numSessions");
+if (!numSessions) localStorage.setItem("numSessions", "4");
 
-let completedTasks = localStorage.getItem('completedTasks');
-if (!completedTasks) localStorage.setItem('completedTasks', '[]');
+let completedTasks = localStorage.getItem("completedTasks");
+if (!completedTasks) localStorage.setItem("completedTasks", "[]");
 
-let upcomingTasks = localStorage.getItem('upcomingTasks');
-if (!upcomingTasks) localStorage.setItem('upcomingTasks', '[]');
+let upcomingTasks = localStorage.getItem("upcomingTasks");
+if (!upcomingTasks) localStorage.setItem("upcomingTasks", "[]");
 
-let WorkOrBreak = localStorage.getItem('WorkOrBreak');
-if (!WorkOrBreak) {
-  localStorage.setItem('WorkOrBreak', 'work');
+let workOrBreak = localStorage.getItem("workOrBreak");
+if (!workOrBreak) {
+  localStorage.setItem("workOrBreak", "work");
 }
-let NumcurrentSech = localStorage.getItem('NumcurrentSech');
-if (!NumcurrentSech) localStorage.setItem('NumcurrentSech', '0');
+let numCurrentSech = localStorage.getItem("numCurrentSech");
+if (!numCurrentSech) localStorage.setItem("numCurrentSech", "0");
--- source/timer.js
+++ source/timer.js
@@ -1,9 +1,9 @@
 //Timer
 
-const countdown = document.getElementById('countdown');
-const title = document.getElementById('title-countdown');
-const startBtn = document.getElementById('start-btn');
-countdown.innerHTML = `${localStorage.getItem('workMins')}:00`;
+const countdown = document.getElementById("countdown");
+const title = document.getElementById("title-countdown");
+const startBtn = document.getElementById("start-btn");
+countdown.innerHTML = `${localStorage.getItem("workMins")}:00`;
 
 /**
 timer
@@ -11,41 +11,41 @@ Uses the countdown h1 to set and run a timer of length designated by the startTi
 */
 
 function timer() {
-  if (startBtn.innerHTML == 'Start') {
-    startBtn.innerHTML = 'cancel';
-    document.getElementById('settings-btn').style.display = 'none';
+  if (startBtn.innerHTML == "Start") {
+    startBtn.innerHTML = "cancel";
+    document.getElementById("settings-btn").style.display = "none";
     updateCountdown(true);
   } else {
     updateCountdown(false);
-    startBtn.innerHTML = 'Start';
+    startBtn.innerHTML = "Start";
   }
 }
 
 function updateCountdown(IsOn) {
-  const devMode = document.getElementById('dev-mode');
+  const devMode = document.getElementById("dev-mode");
   if (IsOn) {
-    if (localStorage.getItem('WorkOrBreak') == 'work') {
-      time = localStorage.getItem('workMins') * 60;
+    if (localStorage.getItem("workOrBreak") == "work") {
+      time = localStorage.getItem("workMins") * 60;
       count = setInterval(updateTime, devMode.checked ? 10 : 1000);
-      localStorage.setItem('intervalID', count);
-    } else if (localStorage.getItem('WorkOrBreak') == 'break') {
-      time = localStorage.getItem('shortBreakMins') * 60;
+      localStorage.setItem("intervalID", count);
+    } else if (localStorage.getItem("workOrBreak") == "break") {
+      time = localStorage.getItem("shortBreakMins") * 60;
       count = setInterval(updateTime, devMode.checked ? 10 : 1000);
-      localStorage.setItem('intervalID', count);
-    } else if (localStorage.getItem('WorkOrBreak') == 'longBreak') {
-      time = localStorage.getItem('longBreakMins') * 60;
+      localStorage.setItem("intervalID", count);
+    } else if (localStorage.getItem("workOrBreak") == "longBreak") {
+      time = localStorage.getItem("longBreakMins") * 60;
       count = setInterval(updateTime, devMode.checked ? 10 : 1000);
-      localStorage.setItem('intervalID', count);
+      localStorage.setItem("intervalID", count);
     }
   } else {
-    document.getElementById('settings-btn').style.display = 'block';
-    clearInterval(localStorage.getItem('intervalID'));
-    if (localStorage.getItem('WorkOrBreak') == 'work') {
-      countdown.innerHTML = `${localStorage.getItem('workMins')}:00`;
-    } else if (localStorage.getItem('WorkOrBreak') == 'break') {
-      countdown.innerHTML = `${localStorage.getItem('shortBreakMins')}:00`;
-    } else if (localStorage.getItem('WorkOrBreak') == 'longBreak') {
-      countdown.innerHTML = `${localStorage.getItem('longBreakMins')}:00`;
+    document.getElementById("settings-btn").style.display = "block";
+    clearInterval(localStorage.getItem("intervalID"));
+    if (localStorage.getItem("workOrBreak") == "work") {
+      countdown.innerHTML = `${localStorage.getItem("workMins")}:00`;
+    } else if (localStorage.getItem("workOrBreak") == "break") {
+      countdown.innerHTML = `${localStorage.getItem("shortBreakMins")}:00`;
+    } else if (localStorage.getItem("workOrBreak") == "longBreak") {
+      countdown.innerHTML = `${localStorage.getItem("longBreakMins")}:00`;
     }
   }
 
@@ -55,36 +55,36 @@ function updateCountdown(IsOn) {
     let mins = Math.floor(time / 60);
     let sec = time % 60;
 
-    sec = sec < 10 ? '0' + sec : sec;
+    sec = sec < 10 ? "0" + sec : sec;
 
     title.innerHTML = `${mins}:${sec}`;
     countdown.innerHTML = `${mins}:${sec}`;
     time--;
     if (time == 0) {
       clearInterval(count);
-      if (localStorage.getItem('WorkOrBreak') == 'work') {
+      if (localStorage.getItem("workOrBreak") == "work") {
         localStorage.setItem(
-          'NumcurrentSech',
-          1 + Number(localStorage.getItem('NumcurrentSech'))
+          "numCurrentSech",
+          1 + Number(localStorage.getItem("numCurrentSech"))
         );
-        console.log(localStorage.getItem('NumcurrentSech'));
+        console.log(localStorage.getItem("numCurrentSech"));
         if (
-          localStorage.getItem('NumcurrentSech') >=
-          localStorage.getItem('numSessions')
+          localStorage.getItem("numCurrentSech") >=
+          localStorage.getItem("numSessions")
         ) {
-          localStorage.setItem('WorkOrBreak', 'longBreak');
+          localStorage.setItem("workOrBreak", "longBreak");
         } else {
-          localStorage.setItem('WorkOrBreak', 'break');
+          localStorage.setItem("workOrBreak", "break");
         }
-      } else if (localStorage.getItem('WorkOrBreak') == 'break') {
-        localStorage.setItem('WorkOrBreak', 'work');
-        startBtn.innerHTML = 'Start';
+      } else if (localStorage.getItem("workOrBreak") == "break") {
+        localStorage.setItem("workOrBreak", "work");
+        startBtn.innerHTML = "Start";
         updateCountdown(false);
         return;
-      } else if (localStorage.getItem('WorkOrBreak') == 'longBreak') {
-        localStorage.setItem('NumcurrentSech', '0');
-        localStorage.setItem('WorkOrBreak', 'work');
-        startBtn.innerHTML = 'Start';
+      } else if (localStorage.getItem("workOrBreak") == "longBreak") {
+        localStorage.setItem("numCurrentSech", "0");
+        localStorage.setItem("workOrBreak", "work");
+        startBtn.innerHTML = "Start";
         updateCountdown(false);
         return;
       }
