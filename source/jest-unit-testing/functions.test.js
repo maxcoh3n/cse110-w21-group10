f38008a277261283a15cfb60773ba36276fe53ad
maxcoh3n <maxcoh3n@gmail.com>
2021-02-25 10:33:45 -0800

    added first actual test

--- source/jest-unit-testing/functions.test.js
+++ source/jest-unit-testing/functions.test.js
@@ -1,10 +1,16 @@
-const functions = require('../functions.js');
+describe('timer ', ()=>{
+  test( ' does not crash', ()=>{
+    document.body.innerHTML =
+    '        <h1 id="countdown"></h1>    ' +
+    '        <button id="start-btn">Start</button>'     +
+    '            <h2>Task List</h2> <button id="complete-task-btn">Completed</button>    ' +
+    '<div id="task-list"> </div>' +
+    '    <button id="settings-btn">Settings</button>    ' +
+    '         <br><br><label for="dev-mode">Dev Mode</label><input type="checkbox" id="dev-mode">  '
+    ;
+    const {timer} = require('../timer'); // must be included after innerHTML is defined or else it will crash
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(functions.functionAdd.add(1, 2)).toBe(3);
-});
-
-test('subtracts 3 - 2 to equal 1', () => {
-  expect(functions.functionSub.subtract(3, 2)).toBe(1);
+    expect(timer()).toBeUndefined();
+  })
 });
 
