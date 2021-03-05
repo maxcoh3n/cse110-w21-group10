fc47a82b7f513bd5cf5c7188ecb1f7fb81f9ac8b
maxcoh3n <maxcoh3n@gmail.com>
2021-03-04 21:43:32 -0800

    add lines that were mistakenly deleted

--- source/taskList.js
+++ source/taskList.js
@@ -227,8 +227,11 @@ function undo() {
             completedSessions[i].completed = false;
           }
         }
+        localStorage.setItem("completedSessions", JSON.stringify(completedSessions));
       }
-      localStorage.setItem("completedSessions", JSON.stringify(completedSessions));
+      if ( box.name == "task-list" ) {
+        taskArray.push(box.id);
+      }
     }
     localStorage.setItem("upcomingTasks", JSON.stringify(taskArray));
     completedTaskDates = new Set();
