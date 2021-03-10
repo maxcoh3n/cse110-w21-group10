8d3935897fa08d78b9e06654697d82f8d859ae2b
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-03-09 20:54:21 -0800

    Create info.js

---
+++ source/info.js
@@ -0,0 +1,19 @@
+//info.js
+
+let infoModal = document.getElementById("info-modal");
+let infoBtn = document.getElementById("info-btn");
+let infoSpan = document.getElementById("info-span");
+
+infoBtn.onclick = function() {
+    infoModal.style.display = "block";
+}
+
+infoSpan.onclick = function() {
+    infoModal.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target == infoModal) {
+        infoModal.style.display = "none";
+    }
+  });
