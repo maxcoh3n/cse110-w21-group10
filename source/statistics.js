41c1ba82e2ed03d06a784b1dc9621e6afd91e389
Jerry Waldorf <jwaldorf@ucsd.edu>
2021-03-10 02:11:04 -0800

    fixed jest hopefully

--- source/statistics.js
+++ source/statistics.js
@@ -1,7 +1,9 @@
-// Calls on page refresh
-renderStatistics();
+// renders statistics is refreshed.
+window.addEventListener("DOMContentLoaded", (event) => {
+  renderStatistics();
+});
 
-function renderStatistics(){
+function renderStatistics() {
   const numCompletedTasks = document.getElementById("num-tasks-completed").childNodes[2];
   const avgSessionsPerDay = document.getElementById("avg-sessions-per-day").childNodes[2];
   const avgTasksPerDay = document.getElementById("average-tasks-per-day").childNodes[2];
@@ -9,27 +11,21 @@ function renderStatistics(){
 
   const statsObj = JSON.parse(localStorage.getItem("statistics"));
 
-
   numCompletedTasks.innerHTML = statsObj.numCompletedTasks;
 
-  if(statsObj.numCompletedTasks >0){
-      avgSessionsPerTask.innerHTML = (statsObj.numCompletedTaskSessions / statsObj.numCompletedTasks).toFixed(2);
-  }else{
-      avgSessionsPerTask.innerHTML = "N/A";
+  if (statsObj.numCompletedTasks > 0) {
+    avgSessionsPerTask.innerHTML = (statsObj.numCompletedTaskSessions / statsObj.numCompletedTasks).toFixed(2);
+  } else {
+    avgSessionsPerTask.innerHTML = "N/A";
   }
 
-  if(statsObj.numDaysWorking >0){
-      avgTasksPerDay.innerHTML = (statsObj.numCompletedTasks / statsObj.numDaysWorking).toFixed(2);
-      avgSessionsPerDay.innerHTML = (statsObj.numSessions / statsObj.numDaysWorking).toFixed(2);
-  }else{
-      avgTasksPerDay.innerHTML = "N/A";
-      avgSessionsPerDay.innerHTML = "N/A";
+  if (statsObj.numDaysWorking > 0) {
+    avgTasksPerDay.innerHTML = (statsObj.numCompletedTasks / statsObj.numDaysWorking).toFixed(2);
+    avgSessionsPerDay.innerHTML = (statsObj.numSessions / statsObj.numDaysWorking).toFixed(2);
+  } else {
+    avgTasksPerDay.innerHTML = "N/A";
+    avgSessionsPerDay.innerHTML = "N/A";
   }
 }
 
-// renders statistics is refreshed.
-window.addEventListener("DOMContentLoaded", (event) => {
-  renderStatistics();
-});
-
-export{renderStatistics};
+export { renderStatistics };
