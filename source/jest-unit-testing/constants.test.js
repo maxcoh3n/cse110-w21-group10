5898b5eb6e268681d1208ba6f5fb83311b8897ff
Max Cohen <maxcoh3n@gmail.com>
2021-03-17 13:13:20 -0700

    continue test

--- source/jest-unit-testing/constants.test.js
+++ source/jest-unit-testing/constants.test.js
@@ -1,4 +1,7 @@
 import {setLocalStorageDefaults} from "../constants.js";
+import {meh} from "../bad.js"
+
+
 describe('constants', ()=>{
     test('testing renderAll when tasksArray length = 0', ()=>{
         expect(setLocalStorageDefaults()).toBeUndefined();
--- source/sounds/bad.js
+++ source/sounds/bad.js
@@ -1,3 +1,9 @@
 function thisFunctionisBad(){
     console.log("I am a bad function I do nothing useful")
 }
+
+function meh(){
+    console.log("meh")
+}
+
+export {meh};
